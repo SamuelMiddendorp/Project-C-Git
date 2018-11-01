42262ef3790c0adea32b77d2d3c75fda3983cef8
Samuel Middendorp <0908398@hr.nl>
2018-11-01 14:42:04 +0100

    Hashing function
    Added hashing helper function to compute sha256 hashing for passwords etc

--- TheRichLifeProject/TheRichLifeProject/Extensions.cs
+++ TheRichLifeProject/TheRichLifeProject/Extensions.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
@@ -23,6 +25,23 @@ namespace TheRichLifeProject
         {
             var value = session.GetString(key);
             return value == null ? default(T) : JsonConvert.DeserializeObject<T>(value);
+        }
+        static string ComputeSha256Hash(string rawData)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder hashbuilder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    hashbuilder.Append(bytes[i].ToString("x2"));
+                }
+                return hashbuilder.ToString();
+            }
         }
     }
 }
